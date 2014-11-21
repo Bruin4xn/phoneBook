cf67b9d5b669c0cc775771cdb94bfc2ea3900a6b
abjia <daohao1914>
2014-11-21 18:30:47 +0800

    添加联系人

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,6 +5,8 @@ var controlModule = angular.module("phoneBook.controllers",[]);
 
     //最近通话记录
     controlModule.controller("callCtrl",function($scope){
+        console.log("最近通话控制器....");
+
         $scope.callList =  [
             {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
             {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
@@ -13,48 +15,43 @@ var controlModule = angular.module("phoneBook.controllers",[]);
             {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
             {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
             {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"}
         ];
-
-
-
-
 
     });
 
     //联系人
     controlModule.controller("contactCtrl",function($scope,utilService){
-        $scope.contactList =  [
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"},
-            {"userName" : "周宁静", "local" : "陕西西安", "operators" : "电信",lastCallTime : "昨天"}
-        ];
+        console.log("联系人控制器.....");
 
+        //通讯录
+        $scope.contactList =  [];
+
+        //表单提交对象
+        $scope.contactObj = {
+            phoneList : []
+        };
+
+        //添加电话号码
+        $scope.addItem = function() {
+            $scope.contactObj.phoneList.push({"phoneNum" : "", "phoneType" : "家里"});
+        };
+
+        //提交表单
+        $scope.subUser = function(){
+            console.log("提交！....");
+            console.log($scope.contactObj);
+            //copy一份
+            var obj = angular.copy($scope.contactObj);
+            //添加
+            $scope.contactList.push(obj);
+            //关闭
+            $scope.closeModal();
+
+        }
         //初始化modal
         utilService.modal($scope,"tpls/contact/add-contact.html");
-
     });
+
 
     controlModule.controller("settingCtrl",function($scope){
         $scope.settingList = [
